e3f7030913d1c4763a238cd16dfd6c6034ba7162
toncy11 <ajagodic1990@gmail.com>
2020-12-08 23:22:37 +0100

    1st example

--- src/main/java/com/concurrency/examples/ConcurrencyExample1.java
+++ src/main/java/com/concurrency/examples/ConcurrencyExample1.java
@@ -1,15 +1,26 @@
 package com.concurrency.examples;
 
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.TimeoutException;
-
 public class ConcurrencyExample1 {
 
-    public static void main(String[] args) throws ExecutionException, InterruptedException, TimeoutException {
-        CompletableFuture<String> completableFuture = new CompletableFuture<String>();
-        completableFuture.get(1, TimeUnit.SECONDS);
-        completableFuture.complete("Future's Result");
-    }
+	public static final String ANSI_RESET = "\u001B[0m";
+	public static final String ANSI_BLACK = "\u001B[30m";
+	public static final String ANSI_RED = "\u001B[31m";
+	public static final String ANSI_GREEN = "\u001B[32m";
+	public static final String ANSI_YELLOW = "\u001B[33m";
+	public static final String ANSI_BLUE = "\u001B[34m";
+	public static final String ANSI_PURPLE = "\u001B[35m";
+	public static final String ANSI_CYAN = "\u001B[36m";
+	public static final String ANSI_WHITE = "\u001B[37m";
+	
+	public static void main(String[] args) {
+		System.out.println(ANSI_RED + "Hello from the main thread." + ANSI_RESET);
+
+		new Thread() {
+			public void run() {
+				System.out.println(ANSI_GREEN + "Hello from the anonymous class thread." + ANSI_RED);
+			}
+		}.start();
+
+		System.out.println(ANSI_RED + "Hello again from the main thread." + ANSI_RED);
+	}
 }
